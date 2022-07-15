78c26f059327e03b282aa3ce8e8faa47d0588e2b
Alex Cheah <alexkcheah@gmail.com>
2022-07-15 17:10:39 +0800

    Added comments for hangmanwincontroller

--- src/main/scala/ch/makery/hangman/view/HangmanWinController.scala
+++ src/main/scala/ch/makery/hangman/view/HangmanWinController.scala
@@ -1,5 +1,6 @@
 package ch.makery.hangman.view
 
+// Import libraries 
 import ch.makery.hangman.MainApp
 import scalafxml.core.macros.sfxml
 import scalafx.Includes._
@@ -12,17 +13,21 @@ import scalafx.scene.text.Text
 import scalafx.stage.Stage
 
 @sfxml
+// Controller for HangmanWin.fxml
 class HangmanWinController(
     wingameBG: ImageView,
     nextButton: Button,
     scoreText: Text
 ) {
 
+    // Text object; used to change text value 
     var scoreDisplay = scoreText
     var dialogStage: Stage = null
 
+    // Set background image 
     wingameBG.image = new Image("file:resources/images/Win.jpg")
 
+    // Game will go to the next level when user click next button
     def goNext(action : ActionEvent): Unit = { 
         MainApp.showGame()
         dialogStage.close()
