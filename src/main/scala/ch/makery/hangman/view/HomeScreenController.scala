47e73f9e2f317fda0429993df49ebf2e90f21921
Alex Cheah <alexkcheah@gmail.com>
2022-07-15 17:05:32 +0800

    Added comments for home screen controller

--- src/main/scala/ch/makery/hangman/view/HomeScreenController.scala
+++ src/main/scala/ch/makery/hangman/view/HomeScreenController.scala
@@ -1,5 +1,6 @@
 package ch.makery.hangman.view
 
+// Import libraries 
 import ch.makery.hangman.MainApp
 import scalafxml.core.macros.sfxml
 import scalafx.Includes._
@@ -10,16 +11,20 @@ import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.media.{Media, MediaPlayer}
 
 @sfxml
+// Controller for HomeScreen.fxml
 class HomeScreenController() {
-    
+
+    // Start the game when player click play button 
     def startGame(action : ActionEvent): Unit = {
         MainApp.showGame()
     }
 
+    // Terminate the program when play click the quit button 
     def endGame(action: ActionEvent): Unit = {
         System.exit(0)
     }
 
+    // Execute a new scene to display the game's instruction when player click the how to play button 
     def showRule(action: ActionEvent): Unit = {
         MainApp.showRule()
     }
